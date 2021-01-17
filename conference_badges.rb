874f5979e40aad5dc31dce240e27ab7c50f79aac
Hatice <haticekaraty@gmail.com>
2021-01-16 21:03:20 -0500

    commit tree 7665d632fdff9be67b0d2548412adbc2047033bb

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,39 @@
 # Write your code here.
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    badge_array =[]
+    array.each do |name|
+        badge_array.push("Hello, my name is #{name}.")
+    end
+    return badge_array
+end
+
+
+def assign_rooms(nameArray)
+    assignedRoomMessages = []
+    nameArray.each_with_index {|name,idx| assignedRoomMessages.push("Hello, #{name}! You'll be assigned to room #{idx+1}!")}
+        #assignedRoomMessages.push("Hello, #{name}! You'll be assigned to room #{roomNum}")
+    return assignedRoomMessages
+end
+
+#assign_rooms(["Haley"])
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |attendee| 
+        puts attendee
+    end
+    assign_rooms(attendees).each do |attendee| 
+        puts attendee
+    end
+    
+end
+
+
+
+
+
+
